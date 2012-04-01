cd337acbe1f5c91a61d70a832d7ac455c151fd7a
Phyo <kophyo48@gmail.com>
2012-04-01 16:40:48 +0100

    reducing requires moving into one file

--- lib/usergrid.rb
+++ lib/usergrid.rb
@@ -1,10 +1,43 @@
 require 'httparty'
-require "./lib/usergrid/version"
-require "./lib/usergrid/client"
+require "usergrid/version"
 
 module Usergrid
-  include HTTParty
-  # Your code goes here...
-  class << self
+  class Client
+    include HTTParty
+    def initialize(options = {})
+      @app = options[:app]
+      @key = options[:key]
+      @secret = options[:secret]
+    end
+
+    def users
+      get("/users")
+    end
+
+    def create_user params
+      post("/users", params)
+    end
+
+    def access_token
+      @access_token ||= self.class.get("#{base_uri}/token?grant_type=client_credentials&client_id=#{@key}&client_secret=#{@secret}").parsed_response["access_token"]
+    end
+
+    def base_uri
+      "http://api.usergrid.com/#{@app}"
+    end
+
+    def get url
+      self.class.get("#{base_uri}#{url}", :headers => headers_hash)
+    end
+
+    def post url, params
+      options = {:body => params.to_json, :headers => {"Content-Type" => "application/json"}}
+      self.class.post("#{base_uri}#{url}", options)
+    end
+
+    def headers_hash
+      {"Authorization" => "Bearer #{access_token}"}
+    end
   end
+
 end
